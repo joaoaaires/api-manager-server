8a64c398125a1a7531a3ce352d3aa344e864bef8
Joao Aires <jantonioayres@gmail.com>
2024-01-28 19:54:08 -0300

    primeiro commit

---
+++ src/main/java/br/com/jaa/api/ApiManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.jaa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManagerApplication.class, args);
+	}
+
+}
