d0c649cb7ed5f5d2cb70d0fe18e5309ecbfe8d0c
Joao Aires <jantonioayres@gmail.com>
2024-02-11 19:06:52 -0300

    ajustado end-points para signIn e signUp, criado README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Projeto
+
+Este é um projeto em Java utilizando o framework Spring Boot. O objetivo deste projeto é unir ferramentas aprendidas em um formato de dicionário e aprendizado.
+
+## Funcionalidades
+
+- Autenticação com JWT
--- src/main/java/br/com/jaa/api/features/IndexController.java
+++ src/main/java/br/com/jaa/api/features/IndexController.java
@@ -1,16 +1,34 @@
 package br.com.jaa.api.features;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping(value = "/")
 public class IndexController {
 
     @GetMapping(value = "")
-    public String index() {
-        return "Hello World";
+    public ResponseEntity<Object> index() {
+        var body = Collections.singletonMap("success", true);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+
+    @PostMapping(value = "/sign-in")
+    public ResponseEntity<Object> signIn() {
+        var body = Collections.singletonMap("signIn", true);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+
+    @PostMapping(value = "/sign-up")
+    public ResponseEntity<Object> signUp() {
+        var body = Collections.singletonMap("signUp", true);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
     }
 
 }
